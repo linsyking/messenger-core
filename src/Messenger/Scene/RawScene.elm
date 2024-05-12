913c2536116b62d69259c186a1c4cc7b1cd290b8
linsyking <kxiang@umich.edu>
2024-05-12 15:20:16 -0400

    feat: raw scene type sugar

--- src/Messenger/Scene/RawScene.elm
+++ src/Messenger/Scene/RawScene.elm
@@ -1,4 +1,7 @@
-module Messenger.Scene.RawScene exposing (genRawScene)
+module Messenger.Scene.RawScene exposing
+    ( RawSceneInit, RawSceneUpdate, RawSceneView
+    , genRawScene
+    )
 
 {-|
 
@@ -7,11 +10,32 @@ module Messenger.Scene.RawScene exposing (genRawScene)
 
 Raw Scene is a scene without anything. Users can add whatever they like in the raw scene!
 
+@docs RawSceneInit, RawSceneUpdate, RawSceneView
 @docs genRawScene
 
 -}
 
-import Messenger.Scene.Scene exposing (MConcreteScene, SceneStorage, abstract)
+import Canvas exposing (Renderable)
+import Messenger.Base exposing (Env, WorldEvent)
+import Messenger.Scene.Scene exposing (MConcreteScene, SceneOutputMsg, SceneStorage, abstract)
+
+
+{-| init type sugar
+-}
+type alias RawSceneInit data userdata scenemsg =
+    Env () userdata -> Maybe scenemsg -> data
+
+
+{-| update type sugar
+-}
+type alias RawSceneUpdate data userdata scenemsg =
+    Env () userdata -> WorldEvent -> data -> ( data, List (SceneOutputMsg scenemsg userdata), Env () userdata )
+
+
+{-| view type sugar
+-}
+type alias RawSceneView userdata data =
+    Env () userdata -> data -> Renderable
 
 
 {-| generate a raw scene from a concrete scene
