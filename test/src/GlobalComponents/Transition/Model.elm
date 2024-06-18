37dc11519ca5e90145a361515df44efbd53c48cb
linsyking <kxiang@umich.edu>
2024-06-18 16:55:43 -0400

    fix: transition gc

--- test/src/GlobalComponents/Transition/Model.elm
+++ test/src/GlobalComponents/Transition/Model.elm
@@ -1,22 +1,71 @@
-module GlobalComponents.Transition.Model exposing (..)
+module GlobalComponents.Transition.Model exposing (InitOption, genGC)
 
+{-|
+
+
+# Transition
+
+@docs InitOption, genGC
+
+-}
+
+import Canvas
 import Dict exposing (Dict)
 import GlobalComponents.Transition.Transitions.Base exposing (TransStorage, Transition)
-import Messenger.Scene.Scene exposing (GlobalComponentInit, MAbstractScene)
+import Json.Encode as E
+import Messenger.Scene.Scene exposing (ConcreteGlobalComponent, GCTarget, GlobalComponentInit, GlobalComponentStorage, GlobalComponentUpdate, GlobalComponentUpdateRec, GlobalComponentView, MAbstractScene, genGlobalComponent)
+
+
+{-| Options
+-}
+type alias InitOption =
+    Dict String TransStorage
 
 
 type alias Data userdata scenemsg =
-    { userTransitions : Dict String TransStorage
+    { userTransitions : InitOption
     , changeSceneTime : Int
     , preScene : Maybe (MAbstractScene userdata scenemsg)
     , transition : Maybe Transition
     }
 
 
-init : Dict String TransStorage -> GlobalComponentInit userdata scenemsg (Data userdata scenemsg)
-init userTransitions env msg =
+init : InitOption -> GlobalComponentInit userdata scenemsg (Data userdata scenemsg)
+init userTransitions _ _ =
     { userTransitions = userTransitions
     , changeSceneTime = 0
     , preScene = Nothing
     , transition = Nothing
     }
+
+
+update : GlobalComponentUpdate userdata scenemsg (Data userdata scenemsg)
+update env _ data =
+    ( data, [], ( env, False ) )
+
+
+updaterec : GlobalComponentUpdateRec userdata scenemsg (Data userdata scenemsg)
+updaterec env _ data =
+    ( data, [], env )
+
+
+view : GlobalComponentView userdata scenemsg (Data userdata scenemsg)
+view _ _ =
+    Canvas.empty
+
+
+gcCon : InitOption -> ConcreteGlobalComponent (Data userdata scenemsg) userdata scenemsg
+gcCon opt =
+    { init = init opt
+    , update = update
+    , updaterec = updaterec
+    , view = view
+    , matcher = "fps"
+    }
+
+
+{-| Generate a global component.
+-}
+genGC : InitOption -> Maybe GCTarget -> GlobalComponentStorage userdata scenemsg
+genGC opt =
+    genGlobalComponent (gcCon opt) E.null
