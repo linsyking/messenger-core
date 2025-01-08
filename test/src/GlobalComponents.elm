8b55376efd872bf28c2d2bbd123015a0c86061e6
linsyking <kxiang@umich.edu>
2025-01-08 16:28:52 -0500

    fix: test

--- test/src/GlobalComponents.elm
+++ test/src/GlobalComponents.elm
@@ -13,6 +13,7 @@ Record all the global components to load at the beginning here.
 
 import Lib.Base exposing (SceneMsg)
 import Lib.UserData exposing (UserData)
+import Messenger.GlobalComponents.FPS.Model as FPS
 import Messenger.Scene.Scene exposing (GlobalComponentStorage)
 
 
@@ -20,4 +21,5 @@ import Messenger.Scene.Scene exposing (GlobalComponentStorage)
 -}
 allGlobalComopnents : List (GlobalComponentStorage UserData SceneMsg)
 allGlobalComopnents =
-    []
+    [ FPS.genGC (FPS.InitOption 30) Nothing
+    ]
--- test/src/Scenes/Transition/Model.elm
+++ test/src/Scenes/Transition/Model.elm
@@ -1,4 +1,4 @@
-module Scenes.Home.Model exposing (scene)
+module Scenes.Transition.Model exposing (scene)
 
 {-| Scene configuration module
 
@@ -11,12 +11,15 @@ import Duration
 import Lib.Base exposing (SceneMsg)
 import Lib.UserData exposing (UserData)
 import Messenger.Base exposing (UserEvent(..))
-import Messenger.GlobalComponents.Transition.Model exposing (genSequentialTransitionSOM)
-import Messenger.GlobalComponents.Transition.Transitions exposing (fadeIn, fadeOut)
+import Messenger.GlobalComponents.Transition.Base exposing (nullTransition)
+import Messenger.GlobalComponents.Transition.Model exposing (genMixedTransitionSOM, genSequentialTransitionSOM)
+import Messenger.GlobalComponents.Transition.Transitions exposing (fadeIn, fadeInWithRenderable, fadeMix, fadeOut)
 import Messenger.Scene.RawScene exposing (RawSceneInit, RawSceneUpdate, RawSceneView, genRawScene)
 import Messenger.Scene.Scene exposing (MConcreteScene, SceneOutputMsg(..), SceneStorage)
+import Quantity
 import REGL
 import REGL.BuiltinPrograms as P
+import String exposing (fromInt)
 
 
 type alias Data =
@@ -33,21 +36,21 @@ update env msg data =
     case msg of
         KeyDown 49 ->
             ( data
-            , [ genSequentialTransitionSOM ( fadeOut, Duration.seconds 1 ) ( fadeIn, Duration.seconds 1 ) ( "Transition", Nothing )
+            , [ genMixedTransitionSOM ( fadeMix, Duration.seconds 1 ) ( "Home", Nothing )
               ]
             , env
             )
 
         KeyDown 50 ->
             ( data
-            , [ SOMChangeScene Nothing "Stress"
+            , [ genSequentialTransitionSOM ( fadeOut, Duration.seconds 1 ) ( fadeIn, Duration.seconds 1 ) ( "Home", Nothing )
               ]
             , env
             )
 
         KeyDown 51 ->
             ( data
-            , [ SOMChangeScene Nothing "Audio"
+            , [ genSequentialTransitionSOM ( nullTransition, Quantity.zero ) ( fadeInWithRenderable <| view env data, Duration.seconds 1 ) ( "Home", Nothing )
               ]
             , env
             )
@@ -56,11 +59,21 @@ update env msg data =
             ( data, [], env )
 
 
+comment : String
+comment =
+    """Mode:
+1: Fade out + Fade in, mixed
+2: Fade out black + Fade in black, sequential
+3: null + Fade in with Renderable, sequential
+"""
+
+
 view : RawSceneView UserData Data
 view env data =
     REGL.group []
-        [ P.clear Color.lightYellow
-        , P.textbox ( 0, 1080 ) 50 "Menu\n1. Transition Test\n2. Rendering Stress Test\n3. Audio Test" "arial" Color.black
+        [ P.clear Color.lightGreen
+        , P.textbox ( 0, 1080 ) 40 comment "arial" Color.black
+        , P.textbox ( 0, 100 ) 30 (fromInt env.globalData.sceneStartFrame) "arial" Color.black
         ]
 
 
